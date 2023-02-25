0017b9bc0c2899e15b0c8e426991f99ec2a64e3d
yjun6 <Su_Xi_03@outlook.com>
2023-02-25 23:09:40 +0800

    第一个java程序

---
+++ java_grammar/hello/src/hello.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:Yu
+ * Date:2023-02-25
+ * Time:19:37
+ */
+
+public class hello{
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
+
