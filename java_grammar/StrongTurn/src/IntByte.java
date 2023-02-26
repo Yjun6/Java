8f7ac842318d3eca8683e507c510fc3b5b8107e5
yjun6 <Su_Xi_03@outlook.com>
2023-02-26 23:28:44 +0800

    int和byte的转换

---
+++ java_grammar/StrongTurn/src/IntByte.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:Yu
+ * Date:2023-02-26
+ * Time:23:05
+ */
+public class IntByte {
+    public static void main(String[] args) {
+        int num1 = 10;
+        byte num2 = (byte)num1;
+        System.out.println(num2);
+        System.out.println("=========");
+
+        byte num3 = 12;
+        int num4 = num3;
+        System.out.println(num4);
+        System.out.println("========");
+    }
+}
