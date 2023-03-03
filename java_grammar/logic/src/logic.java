6ef0f37bcd9574b6cd0d9e0dfbec2b08d42644cc
yjun6 <Su_Xi_03@outlook.com>
2023-03-03 19:53:29 +0800

    控制逻辑语句

---
+++ java_grammar/logic/src/logic.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:Yu
+ * Date:2023-03-02
+ * Time:22:11
+ */
+public class logic {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main1(String[] args) {
+        System.out.println("11\f1111\f1");//‘\f’ -> 两个小的FF
+    }
+}
