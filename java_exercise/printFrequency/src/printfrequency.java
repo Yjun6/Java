e510199d1b3434d2756d818adee11fc36eea5dde
yjun6 <Su_Xi_03@outlook.com>
2023-03-05 20:30:53 +0800

    代码练习

--- java_exercise/printFrequency/src/printfrequency.java
+++ java_exercise/printFrequency/src/printfrequency.java
@@ -8,17 +8,5 @@
 public class printfrequency {
     public static void main(String[] args) {
         //编写程序数一下 1到 100 的所有整数中出现多少个数字9
-        int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            int j = i;
-            while(j >= 9){
-                if(j % 10 == 9){
-                    sum++;
-                }
-                j /= 10;
-            }
-        }
-
-        System.out.println("1-100中9出现的次数为：" + sum);
     }
 }
--- java_grammar/printMultiplicationTable/src/printMultiplicationTable.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;/**
-*Created with IntelliJ IDEA.
-*Description:
-*User:Yu
-*Date:2023-03-05
-*Time:12:59
-*/public class printMultiplicationTable {
-}
