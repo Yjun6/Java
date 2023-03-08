8667a3a8079f23891dfff522110a7bc8f0265d1a
yjun6 <Su_Xi_03@outlook.com>
2023-03-08 23:36:50 +0800

    打印数值中的每一位

--- java_exercise/print_each_bit_of_an_integer/src/printEachBitOfAnInteger.java
+++ java_exercise/print_each_bit_of_an_integer/src/printEachBitOfAnInteger.java
@@ -1,8 +1,25 @@
-package PACKAGE_NAME;/**
-*Created with IntelliJ IDEA.
-*Description:
-*User:Yu
-*Date:2023-03-05
-*Time:13:12
-*/public class printEachBitOfAnInteger {
+import java.util.Scanner;
+
+public class printEachBitOfAnInteger {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int UserInput = scan.nextInt();
+
+        if (UserInput == 0) {
+            System.out.println(UserInput);
+        }else{
+            //排除掉0
+            while((UserInput % 10) == 0){
+                UserInput /= 10;
+            }
+
+            while(UserInput != 0) {
+                System.out.print(UserInput % 10);
+                UserInput /= 10;
+            }
+        }
+
+
+    }
 }
