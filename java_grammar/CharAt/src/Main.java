d922b365be20146891cbabf40f261dce07db4a05
yjun6 <Su_Xi_03@outlook.com>
2023-03-28 23:02:32 +0800

    CharAt使用

---
+++ java_grammar/CharAt/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:${USER}
+ * Date:${YEAR}-${MONTH}-${DAY}
+ * Time:${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        String arr = "abcd";
+        char ch = arr.charAt(1);
+        System.out.println(ch);
+        int index = arr.indexOf('a');
+        System.out.println(index);
+    }
+}
