9d114397c845f746c5218b20d7199bf0dc9a496a
yjun6 <Su_Xi_03@outlook.com>
2023-03-28 23:03:15 +0800

    字符串转小写

---
+++ java_grammar/StringToLowerCase/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:${USER}
+ * Date:${YEAR}-${MONTH}-${DAY}
+ * Time:${TIME}
+ */
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String arr = scanner.nextLine();
+        String arr2 = arr.toLowerCase();
+        System.out.println(arr2);
+    }
+}
