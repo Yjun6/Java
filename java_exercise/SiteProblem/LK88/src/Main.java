92a2b8e2ce05ec777428238894974aad68a3e131
yjun6 <Su_Xi_03@outlook.com>
2023-04-06 21:01:11 +0800

    力扣88

---
+++ java_exercise/SiteProblem/LK88/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:${USER}
+ * Date:${YEAR}-${MONTH}-${DAY}
+ * Time:${TIME}
+ */
+//题解
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2,0,nums1,m,n);
+        Arrays.sort(nums1);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
