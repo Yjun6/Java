0d3ef96d71e6ad0a5a6790599e04b4b769010ca2
yjun6 <Su_Xi_03@outlook.com>
2023-09-16 23:40:34 +0800

    力扣283

---
+++ java_pa/LK_283.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
