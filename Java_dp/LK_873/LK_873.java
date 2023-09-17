fa5adff0df7f46ee437c8761244e0a6b088213cf
yjun6 <Su_Xi_03@outlook.com>
2023-09-17 23:29:34 +0800

    力扣873

---
+++ Java_dp/LK_873/LK_873.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lenLongestFibSubseq(int[] arr) {
+        Map<Integer, Integer> indices = new HashMap<Integer, Integer>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            indices.put(arr[i], i);
+        }
+        int[][] dp = new int[n][n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+                int k = indices.getOrDefault(arr[i] - arr[j], -1);
+                if (k >= 0) {
+                    dp[j][i] = Math.max(dp[k][j] + 1, 3);
+                }
+                ans = Math.max(ans, dp[j][i]);
+            }
+        }
+        return ans;
+    }
+}
