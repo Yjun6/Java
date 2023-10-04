09fab47601d80b76b4ab8a8a0a31fe35c20b862a
yjun6 <Su_Xi_03@outlook.com>
2023-10-04 22:56:01 +0800

    服务器返回一个html数据

---
+++ java_exercise/untitled/src/main/java/BodyServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:Yu
+ * Date:2023-10-04
+ * Time:22:10
+ */
+@WebServlet("/body")
+public class BodyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf8");
+        // 让服务器返回一个html数据
+        resp.getWriter().write("<div>你好</div>");
+    }
+}
