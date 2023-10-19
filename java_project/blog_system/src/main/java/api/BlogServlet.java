c661ed36d1beeff1c759124b52f7e0b867375524
yjun6 <Su_Xi_03@outlook.com>
2023-10-19 22:36:43 +0800

    博客系统servlet

--- java_project/blog_system/src/main/java/api/BlogServlet.java
+++ java_project/blog_system/src/main/java/api/BlogServlet.java
@@ -3,12 +3,14 @@ package api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.Blog;
 import dao.BlogDao;
+import dao.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -50,7 +52,22 @@ public class BlogServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //1.获取请求中的传递的内容
+        //1. 获取到登录的用户的参数
+        // 在博客编辑页，已经做了登录检查，当用户提交时，必然是已经登录状态
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            resp.setContentType("text/html; charset=utf8");
+            resp.getWriter().write("用户未登录，无法发布博客");
+            return;
+        }
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            resp.setContentType("text/html; charset=utf8");
+            resp.getWriter().write("用户未登录，无法发布博客");
+            return;
+        }
+        //2.获取请求中的传递的内容
+        req.setCharacterEncoding("utf8");//遇到中文处理一下
         String title = req.getParameter("title");
         String content = req.getParameter("content");
         if (title == null || content == null
@@ -59,23 +76,15 @@ public class BlogServlet extends HttpServlet {
             resp.getWriter().write("标题或正文为空");
             return;
         }
-        //2.构造Blog对象，并且插入到数据库中
+        //3.构造Blog对象，并且插入到数据库中
         Blog blog = new Blog();
         blog.setTitle(title);
         blog.setContent(content);
+        blog.setUserId(user.getUserId());
+        // 由于在sql插入数据时，已经使用now获取当前时间，不需要此时在手动加入时间
+        BlogDao blogDao = new BlogDao();
+        blogDao.insert(blog);
+        //4. 跳转到博客列表页
+        resp.sendRedirect("blog_list.html");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
