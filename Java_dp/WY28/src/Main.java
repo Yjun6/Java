c6d6accf5b04212cbff6e1b5847c5319c3e77aab
yjun6 <Su_Xi_03@outlook.com>
2023-10-26 23:58:08 +0800

    WY28牛客

--- Java_dp/WY28/src/Main.java
+++ Java_dp/WY28/src/Main.java
@@ -52,3 +52,138 @@ public class Main {
         return list;
     }
 }
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+class Main1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextInt()) { // 注意 while 处理多个 case
+            //     int a = in.nextInt();
+            //     int b = in.nextInt();
+            //     System.out.println(a + b);
+            // }
+            int T = in.nextInt();//表示有几组数据
+            while (T-- > 0) {
+                int n = in.nextInt();//有 2N组数
+                int k = in.nextInt();//洗牌k次
+                int[] num1 = new int[n];
+                int[] num2 = new int[n];
+                for (int i = 0; i < n; i++) {
+                    num1[i] = in.nextInt();
+                }
+                for (int i = 0; i < n; i++) {
+                    num2[i] = in.nextInt();
+                }
+                int[] ret = func(num1, num2);
+                while (k-- > 1) {
+                    for (int i = 0; i < n; i++) {
+                        num1[i] = ret[i];
+                    }
+                    for (int i = n; i < 2 * n; i++) {
+                        num2[i - n] = ret[i];
+                    }
+                    ret = func(num1, num2);
+                }
+                for (int i = 2 * n - 1; i >= 0; i--) {
+                    System.out.print(ret[i] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    public static int[] func(int[] num1, int[] num2) {
+        int len = num1.length;
+        int[] tmp = new int[len * 2];
+        for (int i = len * 2 - 1, j = 0; i > 0 && j < len; j++, i -= 2) {
+            tmp[i] = num1[j];
+            tmp[i - 1] = num2[j];
+        }
+        return tmp;
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextInt()) { // 注意 while 处理多个 case
+            // int a = in.nextInt();
+            // int b = in.nextInt();
+            // System.out.println(a + b);
+            int tmp = in.nextInt();
+            String UD = in.next();
+            char[] UDTmp = UD.toCharArray();
+            int len = UD.length();
+            int change = 1;//当前光标位置
+            if (tmp <= 4) {
+                //不需要翻页
+                for (int i = 1; i <= tmp; i++) {
+                    System.out.print(i);
+                    if (i != tmp) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+                //光标挪动
+                for (int i = 0; i < len; i++) {
+                    if (UDTmp[i] == 'U') {
+                        if (change == 1) {
+                            change = tmp;
+                        } else {
+                            change--;
+                        }
+                    } else if (UDTmp[i] == 'D') {
+                        if (change == tmp) {
+                            change = 1;
+                        } else {
+                            change++;
+                        }
+                    }
+                }
+                System.out.print(change);
+            } else {
+                //需要翻页
+                //判断这个数字是否对4取余
+                boolean r = (tmp % 4) == 0;
+                //光标挪动
+                for (int i = 0; i < len; i++) {
+                    if (UDTmp[i] == 'U') {
+                        if (change == 1) {
+                            change = tmp;
+                        } else {
+                            change--;
+                        }
+                    } else if (UDTmp[i] == 'D') {
+                        if (change == tmp) {
+                            change = 1;
+                        } else {
+                            change++;
+                        }
+                    }
+                }
+                //判断当前change是否在最后那一面 并且没有4
+                if (!r && (tmp - (tmp % 4)) < change) {
+                    for (int i = tmp - (tmp % 4) + 1; i <= tmp; i++) {
+                        System.out.print(i);
+                        if (i != tmp) {
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
+                    System.out.print(change);
+                } else {
+                    int f = change / 4;
+                    for (int i = f * 4 + 1; i <= f * 4; i++) {
+                        System.out.print(i);
+                        if (i != f * 2) {
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
+                    System.out.print(change);
+                }
+            }
+        }
+    }
+}
