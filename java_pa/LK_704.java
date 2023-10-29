793aafe054f6c82aebf491e3d366124a9ed90525
yjun6 <Su_Xi_03@outlook.com>
2023-10-29 22:10:08 +0800

    704

---
+++ java_pa/LK_704.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
