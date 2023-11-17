307d19947374311f317d21e8cb7e059c4ba91fa3
yjun6 <Su_Xi_03@outlook.com>
2023-11-17 22:22:40 +0800

    mybatis的练习

--- java_exercise/mybatis-demo/src/main/java/com/example/demo/mapper/UserInfoMapper.java
+++ java_exercise/mybatis-demo/src/main/java/com/example/demo/mapper/UserInfoMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.model.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -16,9 +17,36 @@ import java.util.List;
 
 @Mapper
 public interface UserInfoMapper {
-    @Select("select * from userinfo")
+//    @Select("select * from userinfo")
     List<UserInfo> selectAll();
 
-    @Select("select * from userinfo where id = #{id}")
-    UserInfo selectOne(Integer id);
+//    @Select("select * from userinfo where id = #{id}")
+//    UserInfo selectOne(Integer id); //不具有通配性
+    UserInfo selectOne(@Param("id") Integer uid);
+
+    UserInfo login(@Param("username") String username,@Param("password") String password);
+
+    List<UserInfo> getAllByOrder(@Param("myOrder") String myOrder);
+
+    int deleteById(@Param("id") Integer id);
+
+    int update(UserInfo userInfo);
+
+    int add(UserInfo userInfo);
+
+    //添加自增主键Id,返回id
+    int insertId(UserInfo userInfo);
+
+    List<UserInfo> getLikeList(@Param("username") String username);
+
+    int add2(UserInfo userInfo);
+
+    int add3(UserInfo userInfo);
+
+    UserInfo getListByWhere(UserInfo userInfo);
+
+    int update2(UserInfo userInfo);
+
+    int delByIds(List<Integer> ids);
+
 }
--- java_exercise/mybatis-demo/src/test/java/com/example/demo/mapper/UserInfoMapperTest.java
+++ java_exercise/mybatis-demo/src/test/java/com/example/demo/mapper/UserInfoMapperTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,10 +36,120 @@ class UserInfoMapperTest {
     void selectAll() {
         List<UserInfo> list = userInfoMapper.selectAll();
         log.info(list.toString());
+        System.out.println(list.toString());
     }
 
     @Test
     void selectOne() {
         log.info(userInfoMapper.selectOne(2).toString());
+    }
+
+    @Test
+    void login() {
+        System.out.println(userInfoMapper.login("admin", "admin"));
+    }
+
+    @Test
+    void getAllByOrder() {
+        System.out.println(userInfoMapper.getAllByOrder("desc"));
+    }
+
+    @Transactional //使用这个注解，不会真正影响数据库
+    @Test
+    void deleteById() {
+        System.out.println(userInfoMapper.deleteById(2));
+        selectAll();
+    }
+
+    @Transactional
+    @Test
+    void update() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(2);
+        userInfo.setUsername("成都");
+        System.out.println(userInfoMapper.update(userInfo));
+        selectAll();
+    }
+
+    @Test
+    void add() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername("何佬");
+        userInfo.setPassword("168453120");
+        userInfo.setPhone("8946513");
+        userInfo.setAge(25);
+        userInfoMapper.add(userInfo);
+        selectAll();
+    }
+
+    @Test
+    void insertId() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername("语佬");
+        userInfo.setPassword("14865");
+        userInfo.setPhone("6513sg");
+        userInfo.setAge(35);
+        userInfoMapper.insertId(userInfo);
+        selectAll();
+    }
+
+    @Test
+    void getLikeList() {
+        List<UserInfo> list = userInfoMapper.getLikeList("i");
+        System.out.println(list.toString());
+    }
+
+    @Transactional
+    @Test
+    void add2() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername("虾虾");
+        userInfo.setPassword("132520");
+        userInfo.setAge(35);
+        userInfo.setGender(1);
+        userInfo.setPhone("58465132");
+        userInfoMapper.add2(userInfo);
+        selectAll();
+    }
+
+    @Test
+    void add3() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername("gashtjh");
+        userInfo.setPassword("132520");
+        userInfo.setAge(35);
+        userInfo.setGender(1);
+        userInfoMapper.add3(userInfo);
+    }
+
+    @Test
+    void getListByWhere() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(11);
+        userInfo.setUsername("gashtjh");
+        userInfo.setPassword("132520");
+        userInfo.setAge(35);
+        userInfo.setGender(1);
+        UserInfo userInfo1 = userInfoMapper.getListByWhere(userInfo);
+        System.out.println(userInfo1.toString());
+    }
+
+    @Test
+    void update2() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(11);
+        userInfo.setUsername("鹤道人");
+        userInfo.setPassword("46523g");
+        userInfoMapper.update2(userInfo);
+    }
+
+    @Transactional
+    @Test
+    void delByIds() {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        userInfoMapper.delByIds(list);
     }
 }
